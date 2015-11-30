67f4e30d9a4660c71a5afa47958a1b257521f9a6
Xuejie "Rafael" Xiao <xxuejie@gmail.com>
2015-11-30 10:06:28 +0800

    Wrap resp in pcall so as to handle error properly

--- redic.lua
+++ redic.lua
@@ -15,6 +15,27 @@ local resty_commit = function(self)
   return self._db:commit_pipeline()
 end
 
+local _wrap_in_pcall = function(...)
+  local success, res = pcall(...)
+  if success then
+    return res, nil
+  else
+    return nil, res
+  end
+end
+
+local resp_call = function(self, ...)
+  return _wrap_in_pcall(self._db.call, self._db, ...)
+end
+
+local resp_queue = function(self, ...)
+  return _wrap_in_pcall(self._db.queue, self._db, ...)
+end
+
+local resp_commit = function(self, ...)
+  return _wrap_in_commit(self._db.commit, self._db, ...)
+end
+
 local providers = setmetatable({}, {
     __index = function (t, k)
       error("provider '" .. k .. "' is unimplemented")
@@ -25,6 +46,12 @@ providers["lua-resty-redis"] = {
   call = resty_call,
   queue = resty_queue,
   commit = resty_commit
+}
+
+providers["resp"] = {
+  call = resp_call,
+  queue = resp_queue,
+  commit = resp_commit
 }
 
 local deduct_provider = function(db, provider)
@@ -39,8 +66,6 @@ end
 local new = function(db, provider)
   provider = deduct_provider(db, provider)
   if not provider then return "", "No provider is specified!" end
-  -- Resp needs no wrapper
-  if provider == "resp" then return db end
 
   local self = {}
   self._db = db
